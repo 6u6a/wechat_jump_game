ff54b2a5bd02505641cb0151de2f838cd81c6537
6u6a <www.6u6a@139.com>
2018-01-14 15:02:22 +0800

    testing
    Signed-off-by: 6u6a <www.6u6a@139.com>

--- wechat_jump_py3.py
+++ wechat_jump_py3.py
@@ -1,6 +1,8 @@
-# -*- coding: utf-8 -*-
+# -*- coding: gbk -*-
 import os
 import time
+import math
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -8,20 +10,12 @@ from PIL import Image
 
 
 def pull_screenshot():
-    os.system('adb shell screencap -p /sdcard/autojump.png')
-    os.system('adb pull /sdcard/autojump.png .')
-
-
-def jump(distance):
-    press_time = distance * 1.35
-    press_time = int(press_time)
-    cmd = 'adb shell input swipe 320 410 320 410 ' + str(press_time)
-    print(cmd)
-    os.system(cmd)
+    os.system('adb shell screencap -p /sdcard/autojump.png') #截图
+    os.system('adb pull /sdcard/autojump.png .')#保存截图到本地
 
 
 fig = plt.figure()
-pull_screenshot()
+pull_screenshot() #截图并保存
 img = np.array(Image.open('autojump.png'))
 im = plt.imshow(img, animated=True)
 
@@ -30,6 +24,21 @@ click_count = 0
 cor = []
 
 
+def jump(distance, pos):
+    press_time = int(distance * 2.093) #涉及到系数的调整
+    print (press_time)
+    press_time = int(random.uniform(press_time - 0.002, press_time + 0.002))
+    print (press_time)
+    cmd = 'adb shell input swipe {x1} {y1} {x2} {y2} {duration}'.format(
+        x1 = pos[0][0],
+        y1 = pos[0][1],
+        x2 = pos[0][0],
+        y2 = pos[0][1],
+        duration=press_time
+    )
+    print(cmd)
+    os.system(cmd)
+
 def update_data():
     return np.array(Image.open('autojump.png'))
 
@@ -37,7 +46,7 @@ def update_data():
 def updatefig(*args):
     global update
     if update:
-        time.sleep(1.5)
+        time.sleep(1.1)
         pull_screenshot()
         im.set_array(update_data())
         update = False
@@ -56,15 +65,16 @@ def on_click(event):
     cor.append(coords)
 
     click_count += 1
-    if click_count > 1:
+    if click_count > 2:
         click_count = 0
-        cor1 = cor.pop()
+        cor3 = cor.pop()#手动设置点击屏幕的位置
         cor2 = cor.pop()
+        cor1 = cor.pop()
 
         distance = (cor1[0][0] - cor2[0][0])**2 + (cor1[0][1] - cor2[0][1])**2
-        distance = distance ** 0.5
+        distance = math.sqrt(distance)
         print('distance = ', distance)
-        jump(distance)
+        jump(distance, cor3)
         update = True
 
 
